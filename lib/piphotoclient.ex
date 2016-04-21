02f76673e94b4660b67c793a9f4b895a4150f184
Werner Erselina <w.erselina@gmail.com>
2016-04-21 13:23:19 +0200

    initialize Project

---
+++ lib/piphotoclient.ex
@@ -0,0 +1,2 @@
+defmodule Piphotoclient do
+end
---
+++ test/piphotoclient_test.exs
@@ -0,0 +1,8 @@
+defmodule PiphotoclientTest do
+  use ExUnit.Case
+  doctest Piphotoclient
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
